2c048b26e660959a541d60cecf33ef2145b6b813
jiangwenping <jwpttcg66>
2021-09-01 17:57:35 +0800

    工程转化为spring boot工程

--- src/main/java/MyClient.java
+++ src/main/java/MyClient.java
@@ -4,14 +4,14 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
 public class MyClient {
-    public static void main(String[] args) {
-        //使用usePlaintext，否则使用加密连接
-        ManagedChannelBuilder<?> channelBuilder = ManagedChannelBuilder.forAddress("localhost", 8899).usePlaintext();
-        ManagedChannel channel = channelBuilder.build();
-
-        GreeterGrpc.GreeterBlockingStub blockingStub = GreeterGrpc.newBlockingStub(channel);
-        HelloWorldProtos.HelloReply helloReply = blockingStub.sayHello(HelloWorldProtos.HelloRequest.newBuilder().setMessage("hello wolrd").build());
-        System.out.println(helloReply.getMessage());
-    }
+//    public static void main(String[] args) {
+//        //使用usePlaintext，否则使用加密连接
+//        ManagedChannelBuilder<?> channelBuilder = ManagedChannelBuilder.forAddress("localhost", 8899).usePlaintext();
+//        ManagedChannel channel = channelBuilder.build();
+//
+//        GreeterGrpc.GreeterBlockingStub blockingStub = GreeterGrpc.newBlockingStub(channel);
+//        HelloWorldProtos.HelloReply helloReply = blockingStub.sayHello(HelloWorldProtos.HelloRequest.newBuilder().setMessage("hello wolrd").build());
+//        System.out.println(helloReply.getMessage());
+//    }
 
 }
