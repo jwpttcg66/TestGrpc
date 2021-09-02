8a7080469388db0eee81a13c943a28cd898d85af
jiangwenping <jwpttcg66>
2021-09-02 18:44:49 +0800

    为了测试方便暂时取消spring boot打包方式

--- src/main/java/MyServer.java
+++ src/main/java/MyServer.java
@@ -10,18 +10,18 @@ public class MyServer {
     //   启动参数-p8899
     public static void main(String[] args) throws ParseException {
 
-        System.out.println("启动");
+//        System.out.println("启动");
+//
+//        Options options = new Options();
+//        options.addRequiredOption("p", "port", true, "Port to listen or send event to.");
+//
+//        CommandLineParser parser = new DefaultParser();
+//        CommandLine cmd = parser.parse(options, args);
+//
+//        // If port string is not valid, it will throw an exception.
+//        int port = Integer.parseInt(cmd.getOptionValue("port"));
 
-        Options options = new Options();
-        options.addRequiredOption("p", "port", true, "Port to listen or send event to.");
-
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = parser.parse(options, args);
-
-        // If port string is not valid, it will throw an exception.
-        int port = Integer.parseInt(cmd.getOptionValue("port"));
-
-//        int port = 8899;
+        int port = 8899;
         System.out.println("端口" + port);
         ServerBuilder<?> serverBuilder = ServerBuilder.forPort(port);
         serverBuilder.addService(new MyService());
