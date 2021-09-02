3842ec06798f857badf00c7c14b7eb7fa907ab4e
jiangwenping <jwpttcg66>
2021-09-02 11:55:46 +0800

    1·

--- src/main/java/MyServer.java
+++ src/main/java/MyServer.java
@@ -1,15 +1,26 @@
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.protobuf.services.ProtoReflectionService;
+import org.apache.commons.cli.*;
 
 import java.io.IOException;
 
 public class MyServer {
 
-    private static final String GRPC_PORT = System.getenv("DAPR_GRPC_PORT");
-    public static void main(String[] args) {
+    //   启动参数-p8899
+    public static void main(String[] args) throws ParseException {
+
         System.out.println("启动");
-        int port = Integer.parseInt(GRPC_PORT);
+
+        Options options = new Options();
+        options.addRequiredOption("p", "port", true, "Port to listen or send event to.");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        // If port string is not valid, it will throw an exception.
+        int port = Integer.parseInt(cmd.getOptionValue("port"));
+
 //        int port = 8899;
         System.out.println("端口" + port);
         ServerBuilder<?> serverBuilder = ServerBuilder.forPort(port);
