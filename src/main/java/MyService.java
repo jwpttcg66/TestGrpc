e5d7ecd96dcdc20f3e2c25762821c8aaf01cd353
jiangwenping <jwpttcg66>
2021-09-07 12:27:21 +0800

    增加protobuf-java-format

--- src/main/java/MyService.java
+++ src/main/java/MyService.java
@@ -1,3 +1,4 @@
+import com.googlecode.protobuf.format.JsonFormat;
 import com.mattie.grpc.GreeterGrpc;
 import com.mattie.grpc.HelloWorldProtos;
 import io.grpc.stub.StreamObserver;
@@ -6,7 +7,17 @@ public class MyService extends GreeterGrpc.GreeterImplBase {
     @Override
     public void sayHello(HelloWorldProtos.HelloRequest request, StreamObserver<HelloWorldProtos.HelloReply> responseObserver) {
         HelloWorldProtos.HelloReply helloReply = HelloWorldProtos.HelloReply.newBuilder().setMessage("hello client.").build();
-        System.out.println("hello client");
+//        try {
+//            String helloReplyFormat = JsonFormat.printer().print(helloReply);
+//            System.out.println("hello client : " + helloReplyFormat);
+//        }catch (Exception e) {
+//            System.out.println(e);
+//        }
+        JsonFormat format = new JsonFormat();
+        String json = format.printToString(helloReply);
+        System.out.println("hello client : " + json);
+
+
         responseObserver.onNext(helloReply);
         responseObserver.onCompleted();
     }
